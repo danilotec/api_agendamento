bf813828229c571f4accf4aa9136cc2f4a695751
Danilotec <danilo0123499@gmail.com>
2024-12-24 15:06:19 -0300

    add appointments

--- barbershop-scheduler/app/routes.py
+++ barbershop-scheduler/app/routes.py
@@ -28,3 +28,29 @@ def add_barber():
     db.session.add(new_barber)
     db.session.commit()
     return jsonify({"message": "Barbeiro adicionado com sucesso!"}), 201
+
+@main.route('/appointments', methods=['GET'])
+def get_appointments():
+    appointments = Appointment.query.all()
+    result = [
+        {
+            "id": appointment.id,
+            "client_name": appointment.client_name,
+            "barber_id": appointment.barber_id,
+            "appointment_time": appointment.appointment_time
+        }
+        for appointment in appointments
+    ]
+    return jsonify(result)
+
+@main.route('/appointments', methods=['POST'])
+def add_appointment():
+    data = request.get_json()
+    new_appointment = Appointment(
+        client_name=data['client_name'], # type: ignore
+        barber_id=data['barber_id'], # type: ignore
+        appointment_time=data['appointment_time'] # type: ignore
+    )
+    db.session.add(new_appointment)
+    db.session.commit()
+    return jsonify({"message": "Agendamento criado com sucesso!"}), 201
